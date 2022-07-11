3ef93ec141d09320da04e5d52277d2f073529bb2
Marcos Costa <mcosta@divisionsinc.com>
2022-07-10 22:23:43 -0300

    feat: add boilerplate code

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -1,9 +1,11 @@
 module api.Program
 
+open System
 open Falco
 open Falco.Routing
 open Falco.HostBuilder
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
 
 // ------------
 // Exception Handler
@@ -11,7 +13,20 @@ open Microsoft.AspNetCore.Builder
 let exceptionHandler : HttpHandler =
     Response.withStatusCode 500 
     >> Response.ofPlainText "Server error"
-
+let single : HttpHandler =
+    let formBinder (f: FormCollectionReader) : IFormFile option =
+        f.TryGetFormFile "image"
+        
+    let upload (image: IFormFile option) : HttpHandler =
+        let name =
+            image
+            |> Option.bind (fun i -> Some i.Name)
+            |> Option.defaultValue "NoImage"
+        
+        Response.ofPlainText name
+    
+    Request.mapFormStream formBinder upload
+    
 [<EntryPoint>]
 let main args =   
     webHost args {
@@ -19,7 +34,7 @@ let main args =
         use_ifnot FalcoExtensions.IsDevelopment (FalcoExtensions.UseFalcoExceptionHandler exceptionHandler)
         
         endpoints [            
-            get "/" (Response.ofPlainText "Hello world")
+            get "/" single
         ]
     }
     0
